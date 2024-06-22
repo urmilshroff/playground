bc69cc9060180f12af98db431496c0713ee0ffd0
urmilshroff <urmilshroff@gmail.com>
2024-06-22 17:38:35 +0530

    refactor: rename classes

--- lib/counter_page.dart
+++ lib/counter_page.dart
@@ -2,31 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:playground/counter_cubit.dart';
 
-class CounterPage extends StatelessWidget {
-  const CounterPage({super.key});
+class CounterPageHandler extends StatelessWidget {
+  const CounterPageHandler({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CounterCubit(),
-      child: const CounterView(),
+      child: const CounterPage(),
     );
   }
 }
 
-class CounterView extends StatelessWidget {
-  const CounterView({super.key});
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: BlocBuilder<CounterCubit, int>(builder: (context, state) {
-          return Text(
-            state.toString(),
-            style: const TextStyle(fontSize: 32),
-          );
-        }),
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text(
+              state.toString(),
+              style: const TextStyle(fontSize: 32),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
